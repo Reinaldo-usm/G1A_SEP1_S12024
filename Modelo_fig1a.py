9a727d291702721c8fe99b1b772bf4c74b6d6161
Reinaldo-usm <reinaldo.zapata@usm.cl>
2024-06-19 16:48:28 -0400

    Modificación modelo fig1

--- Modelo_fig1a.py
+++ Modelo_fig1a.py
@@ -1,17 +1,29 @@
 import pandapower as pp
 import pandapower.plotting.plotly as pplot
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Crear una red vacía
 net = pp.create_empty_network()
+
 # Crear un nuevo tipo de línea
 line_data = {"r_ohm_per_km": 0.02, "x_ohm_per_km": 0.115, "c_nf_per_km": 19.1, "max_i_ka": 1., "type": "line"}
 pp.create_std_type(net, line_data, "linea")
+
+# Crear barras (buses)
 b1 = pp.create_bus(net, vn_kv=500.)
 b2 = pp.create_bus(net, vn_kv=500.)
+
 # Crear una línea utilizando el nuevo tipo de línea
-pp.create_line(net, from_bus=b1, to_bus=b2, length_km=500., std_type="linea")
-pp.create_line(net, from_bus=b1, to_bus=b2, length_km=500., std_type="linea")   
-#s_mva=1200.
-#pf=0.9
-#p_mw=s_mva*pf
-#q_mvar=s_mva*(1.-pf)
-#pp.create_load(net, bus=b2, p_mw=p_mw, q_mvar=q_mvar)
-pplot.simple_plotly(net)
+linea1 = pp.create_line(net, from_bus=b1, to_bus=b2, length_km=500., std_type="linea", name="linea1", parallel=2)
+
+# Crear una barra externa
+pp.create_ext_grid(net, bus=b1, vm_pu=1., name="barra_ext")
+
+# Valores nominales de carga (puedes ajustarlos según sea necesario)
+p_nom = 1080  # MW
+q_nom = 522.3   # MVAR
+
+# Crear carga inicial
+pp.create_load(net, bus=b2, p_mw=p_nom, q_mvar=q_nom, name="carga1")
+pplot.simple_plotly(net, bus_size=1, line_width=2, filename="fig1a.html")
