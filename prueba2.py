a2ce02069f12161c5afb0e61e4bc2bd170d9dbfe
Reinaldo-usm <reinaldo.zapata@usm.cl>
2024-06-19 00:54:58 -0400

    Codigo final

--- prueba2.py
+++ prueba2.py
@@ -8,10 +8,6 @@ b1 = pp.create_bus(net, vn_kv=500.)
 b2 = pp.create_bus(net, vn_kv=500.)
 # Crear una línea utilizando el nuevo tipo de línea
 pp.create_line(net, from_bus=b1, to_bus=b2, length_km=500., std_type="linea")
-pp.create_line(net, from_bus=b1, to_bus=b2, length_km=500., std_type="linea")   
-#s_mva=1200.
-#pf=0.9
-#p_mw=s_mva*pf
-#q_mvar=s_mva*(1.-pf)
-#pp.create_load(net, bus=b2, p_mw=p_mw, q_mvar=q_mvar)
+#pp.create_line(net, from_bus=b1, to_bus=b2, length_km=500., std_type="linea")   
+pp.create_load(net, bus=b2, p_mw=1080., q_mvar=120., name="carga")
 pplot.simple_plotly(net)
